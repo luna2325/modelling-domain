401e8af0b5e47c8aae8895dda56a2fcb45a987ae
shima <shima@DESKTOP-EGM1I46>
2021-11-04 20:42:35 +0200

    Add project files.

---
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	double x;
+	int sum = 0 ;
+	double product = 1;
+	cout << "input n>= 4 , n =";
+
+	cin >> n;
+
+	cout << "enter x";
+	cin >> x;
+
+	if (x < 0)
+	{
+
+		for (int j = 2; j <= n - 2; j++)
+		{
+			product *= (j + x);
+		}
+		cout << "product =" << product << endl;
+	}
+	else
+	{
+
+		for (int i = 0; i <= n - 1; i++)
+		{
+			double p = 1;
+			for (int j = 0; j <= n - 1; j++)
+			{
+				p *= (x + i + pow(j, 2));
+			}
+			sum += p;
+
+		}
+		cout << "SUM =" << sum << endl;
+	}
+
+	system("pause");
+
+	return 0;
+
+}
